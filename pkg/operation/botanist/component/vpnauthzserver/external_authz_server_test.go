140ab3b781566de016fc783f213e514f2cf81255
Shafeeque E S <shafeeque.e.s@sap.com>
2022-08-25 16:23:05 +0530

    Handle PodDisruptionBudget policy/v1

--- pkg/operation/botanist/component/vpnauthzserver/external_authz_server_test.go
+++ pkg/operation/botanist/component/vpnauthzserver/external_authz_server_test.go
@@ -303,7 +303,7 @@ var _ = Describe("ExtAuthzServer", func() {
 	}
 
 	JustBeforeEach(func() {
-		defaultDepWaiter = New(c, namespace, image, replicas)
+		defaultDepWaiter = New(c, namespace, image, replicas, "")
 	})
 
 	Describe("#Deploy", func() {
